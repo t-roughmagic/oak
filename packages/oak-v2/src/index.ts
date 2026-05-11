e545373af84c9c95b7aa7491d89f25c2bdb7de84
roughmagic <thomas@roughmagic.io>
2026-05-11 13:40:28 -0500

    clean out old v3

--- packages/oak-v2/src/index.ts
+++ packages/oak-v2/src/index.ts
@@ -1,6 +1,5 @@
 export { Cell, type CellOptions } from './cell.js'
 export { makeOak, makeOakLayer, type OakProgram, type OakTag } from './oak.js'
-export * as v3 from './v3/index.js'
 export * as v4 from './v4/index.js'
 export {
   OakRuntimeContext,
