1e8c32ce73274373bd734b69addecf57a124c9e6
roughmagic <thomas@roughmagic.io>
2026-05-17 21:21:26 -0500

    Migrate example-prog-counter to examples/
    Port the counter program from packages/example-prog-counter to
    examples/example-prog-counter using @oak/oak-core + @oak/oak-platform-effect,
    reshape the legacy [Mutation, Cmd[]] return to { mutation, effects }, and
    delete the now-duplicate packages/ copy.

---
+++ examples/example-prog-counter/src/index.ts
@@ -0,0 +1,17 @@
+import { makeOakEffectProgram } from '@oak/oak-platform-effect'
+import { init, type CounterModel } from './model.js'
+import type { CounterMsg } from './message.js'
+import { update } from './update.js'
+
+export type { CounterModel } from './model.js'
+export { CounterMsg } from './message.js'
+
+export function makeCounterProgram(initial: CounterModel = init) {
+  return makeOakEffectProgram<CounterModel, CounterMsg>({
+    tagKey: '@oak/example-prog-counter/CounterProgram',
+    init: initial,
+    update,
+  })
+}
+
+export const counter = makeCounterProgram()
---
+++ examples/example-prog-counter/src/update.ts
@@ -0,0 +1,15 @@
+import * as Optic from '@fp-ts/optic'
+import type { Update } from '@oak/oak-core'
+import type { EffectCommand } from '@oak/oak-platform-effect'
+import type { CounterModel } from './model.js'
+import { CounterMsg } from './message.js'
+
+const _count = Optic.id<CounterModel>().at('count')
+
+type CounterCmd = EffectCommand<CounterModel, CounterMsg>
+
+export const update: Update<CounterModel, CounterMsg, CounterCmd> = (msg) =>
+  CounterMsg.$match(msg, {
+    Increment: () => ({ mutation: Optic.modify(_count)((n) => n + 1), effects: [] }),
+    Decrement: () => ({ mutation: Optic.modify(_count)((n) => n - 1), effects: [] }),
+  })
